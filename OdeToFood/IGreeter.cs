92c8efce416f30148498b34e541181cbf6414ea3
Leo <lsegars@gmail.com>
2018-10-09 06:40:09 -0300

    Add project files.

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration congigutarion)
+        {
+            _configuration = congigutarion;
+        }
+
+        public string GetMessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
