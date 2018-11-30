f63342ba0e4189db2ab516e0e7c65bbdf7c2400f
Leo <lsegars@gmail.com>
2018-11-30 06:31:41 -0300

    5. Using the Entity Framework OK

---
+++ OdeToFood/Data/OdoToFoodDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public class OdoToFoodDbContext : DbContext
+    {
+        public OdoToFoodDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestorantData
+    {
+        private readonly OdoToFoodDbContext _context;
+
+        public SqlRestaurantData(OdoToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public Restaurant Add(Restaurant resto)
+        {
+            _context.Restaurants.Add(resto);
+            _context.SaveChanges();
+            return resto;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(f => f.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.AsEnumerable();
+        }
+    }
+}
